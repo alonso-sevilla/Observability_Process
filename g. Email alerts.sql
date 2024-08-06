85013621aac66c659cef7db06954127b92f27501
Alonso_Sevilla_HKKD <161654691+alonso-sevilla@users.noreply.github.com>
2024-08-06 08:09:01 -0600

    Update g. Email alerts.sql
    Added chris to receits

--- g. Email alerts.sql
+++ g. Email alerts.sql
@@ -1,9 +1,10 @@
 call OBSERVABILITY_SETUP.SETUP_PROCEDURES_V1.EMAIL_LAST_RESULTS(
     'alonso.sevilla@wahlclipper.com',
+    'chris.newgard@wahlclipper.com',
     'david.nelsen@wahlclipper.com',
     'Monitoring Process - New Alerts'
 );
-CREATE OR REPLACE PROCEDURE OBSERVABILITY_SETUP.SETUP_PROCEDURES_V1.EMAIL_LAST_RESULTS("SEND_TO1" VARCHAR(16777216), "SEND_TO2" VARCHAR(16777216), "SUBJECT" VARCHAR(16777216))
+CREATE OR REPLACE PROCEDURE OBSERVABILITY_SETUP.SETUP_PROCEDURES_V1.EMAIL_LAST_RESULTS("SEND_TO1" VARCHAR(16777216), "SEND_TO2" VARCHAR(16777216), "SEND_TO3" VARCHAR(16777216), "SUBJECT" VARCHAR(16777216))
 RETURNS VARCHAR(16777216)
 LANGUAGE PYTHON
 RUNTIME_VERSION = '3.8'
@@ -13,7 +14,7 @@ EXECUTE AS CALLER
 AS '
 import snowflake
 
-def x(session, send_to1, send_to2, subject):
+def x(session, send_to1, send_to2, send_to3, subject):
   session.sql("use role observability_admin").collect()
   session.sql("use warehouse observability_wh").collect()
   query_db = session.sql("SELECT CURRENT_ACCOUNT()|| ''_OBSERVABILITY.OBSERVABILITY_CORE.OBSERVABILITY_METRICS''").to_pandas().iloc[0]["CURRENT_ACCOUNT()|| ''_OBSERVABILITY.OBSERVABILITY_CORE.OBSERVABILITY_METRICS''"]
@@ -22,6 +23,6 @@ def x(session, send_to1, send_to2, subject):
   except snowflake.snowpark.exceptions.SnowparkSQLException as e:
       body = ''%s\\n%s'' % (type(e), e)
 
-  session.call(''system$send_email'', ''alert_infra'', f''{send_to1}, {send_to2}'', subject, body)
+  session.call(''system$send_email'', ''alert_infra'', f''{send_to1}, {send_to2}, {send_to3}'', subject, body)
   return ''email sent:\\n%s'' % body
 ';
